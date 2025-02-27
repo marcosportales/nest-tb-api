de6dde50a21272abcbe556a30b97355c5bdba040
Marcos Portales <marcosportales2002@gmail.com>
2025-02-26 19:57:18 -0500

    feat: import necesary modules, services and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ThingsboardGateway } from '@/ws/thingsboard.gateway';
+import { ConfigModule } from '@nestjs/config';
+import { DbController } from '@/db/db.controller';
+import { DbService } from '@/db/db.service';
+import { AxiosConfigService } from './axios.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Medicion } from '@/entities/medicion.entity';
+import { Variable } from '@/entities/variable.entity';
+import { AuthService } from '@/auth/auth.service';
+import { AuthController } from '@/auth/auth.controller';
+import { VariableService } from './db/variable/variable.service';
+import { MedicionService } from './db/medicion/medicion.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [Variable, Medicion],
+      autoLoadEntities: true,
+    }),
+    TypeOrmModule.forFeature([Variable, Medicion]),
+  ],
+  providers: [
+    ThingsboardGateway,
+    DbService,
+    AuthService,
+    AxiosConfigService,
+    VariableService,
+    MedicionService,
+  ],
+  controllers: [DbController, AuthController],
 })
 export class AppModule {}
