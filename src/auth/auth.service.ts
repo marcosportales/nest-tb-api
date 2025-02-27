6341a868c7d7465ac4e1651576793ca6b8926d77
Marcos Portales <marcosportales2002@gmail.com>
2025-02-26 19:57:49 -0500

    feat: create auth controller and service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,41 @@
+import { AxiosConfigService } from '@/axios.config';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AxiosInstance } from 'axios';
+
+@Injectable()
+export class AuthService {
+  private axios_instance: AxiosInstance;
+  private access_token: string | null;
+
+  constructor(
+    private configService: ConfigService,
+    private axios_config_service: AxiosConfigService,
+  ) {
+    this.axios_instance = this.axios_config_service.getAxiosInstance();
+  }
+
+  get_access_token() {
+    return this.access_token;
+  }
+
+  async login() {
+    const TB_LOGIN_URL = this.configService.get<string>('TB_LOGIN_URL');
+    const username = this.configService.get<string>('TB_USERNAME');
+    const password = this.configService.get<string>('TB_PASSWORD');
+
+    const response = await this.axios_instance.post(TB_LOGIN_URL, {
+      username,
+      password,
+    });
+
+    return response.status === 200 ? response.data.token : null;
+  }
+
+  async updateAccessToken() {
+    const token = await this.login();
+    this.access_token = token;
+    console.log('JWT updated successfully');
+    return token;
+  }
+}
