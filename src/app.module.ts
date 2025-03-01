4bb0a018164373f2787176949e676389ecfbf89f
Marcos Portales <marcosportales2002@gmail.com>
2025-03-01 18:25:07 -0500

    refactor: reorganize module imports and providers in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,29 @@
 import { Module } from '@nestjs/common';
-import { ThingsboardGateway } from '@/ws/thingsboard.gateway';
-import { ConfigModule } from '@nestjs/config';
-import { DbController } from '@/db/db.controller';
-import { DbService } from '@/db/db.service';
-import { AxiosConfigService } from './axios.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Medicion } from '@/entities/medicion.entity';
-import { Variable } from '@/entities/variable.entity';
-import { AuthService } from '@/auth/auth.service';
-import { AuthController } from '@/auth/auth.controller';
-import { VariableService } from './db/variable/variable.service';
-import { MedicionService } from './db/medicion/medicion.service';
+import { ConfigModule } from '@nestjs/config';
+
+import { ThingsboardGateway } from '@/ws/thingsboard.gateway';
+import { AxiosConfigService } from './axios.config';
+import { Medicion } from '@/db/entities/medicion.entity';
+import { Variable } from '@/db/entities/variable.entity';
+import { DbModule } from '@/db/db.module';
+import { AuthModule } from '@/auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
       entities: [Variable, Medicion],
       autoLoadEntities: true,
     }),
-    TypeOrmModule.forFeature([Variable, Medicion]),
+    DbModule,
+    AuthModule,
   ],
-  providers: [
-    ThingsboardGateway,
-    DbService,
-    AuthService,
-    AxiosConfigService,
-    VariableService,
-    MedicionService,
-  ],
-  controllers: [DbController, AuthController],
+  providers: [ThingsboardGateway, AxiosConfigService],
+  exports: [AxiosConfigService],
 })
 export class AppModule {}
