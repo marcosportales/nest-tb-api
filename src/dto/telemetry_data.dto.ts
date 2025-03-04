425a123c954a4d838e424889241a7ee604b0c446
Marcos Portales <marcosportales2002@gmail.com>
2025-03-04 14:26:28 -0500

    refactor: rename Medicion and Variable services and entities

--- src/dto/telemetry_data.dto.ts
+++
@@ -1,8 +0,0 @@
-export interface ITelemetryData {
-  subscriptionId: number;
-  errorCode: number;
-  errorMessage: string | null;
-  data: { [variable: string]: Array<Array<number | string>> };
-  latestValues: { [variable: string]: number };
-  tsStart?: number;
-}
