a4e5a5cb931fa68d148eaa0570cbc4b9b067e6d1
Marcos Portales <marcosportales2002@gmail.com>
2025-03-04 14:23:38 -0500

    refactor: update DbModule with renamed services and added controllers

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DbService } from './db.service';
-import { VariableService } from './variable/variable.service';
-import { MedicionService } from './medicion/medicion.service';
+import { VariablesService } from './variables/variables.service';
+import { MedicionesService } from './mediciones/mediciones.service';
 import { Medicion } from '@/db/entities/medicion.entity';
 import { Variable } from '@/db/entities/variable.entity';
+import { VariablesController } from '@/db/variables/variables.controller';
+import { MedicionesController } from '@/db/mediciones/mediciones.controller';
 
 @Module({
+  controllers: [MedicionesController, VariablesController],
   imports: [TypeOrmModule.forFeature([Variable, Medicion])],
-  providers: [DbService, VariableService, MedicionService],
-  exports: [VariableService, MedicionService, DbService],
+  providers: [DbService, VariablesService, MedicionesService],
+  exports: [VariablesService, MedicionesService, DbService],
 })
 export class DbModule {}
