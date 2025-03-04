548350ccca3edd0318d5e98782872306441f5134
Marcos Portales <marcosportales2002@gmail.com>
2025-03-04 14:20:44 -0500

    feat: configure TypeORM logging and migration settings in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-
 import { ThingsboardGateway } from '@/ws/thingsboard.gateway';
 import { AxiosConfigService } from './axios.config';
-import { Medicion } from '@/db/entities/medicion.entity';
-import { Variable } from '@/db/entities/variable.entity';
 import { DbModule } from '@/db/db.module';
 import { AuthModule } from '@/auth/auth.module';
+import { Variable } from '@/db/entities/variable.entity';
+import { Medicion } from '@/db/entities/medicion.entity';
 
 @Module({
   imports: [
@@ -19,6 +18,10 @@ import { AuthModule } from '@/auth/auth.module';
       url: process.env.DATABASE_URL,
       entities: [Variable, Medicion],
       autoLoadEntities: true,
+      logging: false,
+      synchronize: false,
+      migrations: ['dist/db/migrations/*{.ts,.js}'],
+      migrationsRun: true,
     }),
     DbModule,
     AuthModule,
