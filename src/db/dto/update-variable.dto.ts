d2b9b95d5ccc514048f48c6b45cad8d3f6c636a4
Marcos Portales <marcosportales2002@gmail.com>
2025-03-26 09:15:33 -0400

    fix: remove nombre of UpdateVariableDto since is not allowed it to change it

--- src/db/dto/update-variable.dto.ts
+++ src/db/dto/update-variable.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateVariableDto } from '@/db/dto/create-variable.dto';
 
-export class UpdateVariableDto extends PartialType(CreateVariableDto) {}
+export class UpdateVariableDto extends PartialType(
+  OmitType(CreateVariableDto, ['nombre']),
+) {}
